825799884947e6ea55d7944da5d2535681972ea3
Yozha <130782610+RealYozha@users.noreply.github.com>
2024-01-19 19:01:40 +0300

    Update MainFramework.lua

--- MainFramework.lua
+++ MainFramework.lua
@@ -1,3 +1,7 @@
-local MainFramework = {};
+local returningValue;
 
-return MainFramework;
+if game.ReplicatedStorage.YozhaFwCfg then
+    return game:GetService"HttpService":GetAsync"https://raw.githubusercontent.com/RealYozha/Roblox-YozhaFramework-MainFramework/main/MainFramework.lua";
+else
+    return game:GetService"HttpService":GetAsync"https://raw.githubusercontent.com/RealYozha/Roblox-YozhaFramework-MainFramework/main/MainFramework.lua";
+end;
